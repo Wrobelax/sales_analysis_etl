40346c40c66a58d03967a8e4070c167be65db622
Adrian <awrobel.wr@gmail.com>
2025-07-23 18:32:13 +0200

    Completed data loading and data cleaning. Exported new table to database.

--- etl/load_data.py
+++ etl/load_data.py
@@ -1,20 +1,53 @@
-"""Script for loading data into pandas DataFrames and SQL"""
+"""Script for loading data into pandas DataFrames and SQL including data cleaning"""
 
 # Importing required modules
 import pandas as pd
 import sqlite3 as sql
-
-# Importing data to DF and SQL
-df = pd.read_csv('../data/e-commerce_data.csv', encoding = "ISO-8859-1")
-
-conn = sql.connect('../data/e-commerce.db')
-df.to_sql('orders', conn , if_exists='replace', index = False)
-
-result_df = pd.read_sql('SELECT * FROM orders LIMIT 10', conn)
+import numpy as np
 
 
+
+"""Loading data"""
+# Importing data to DF and SQL.
+try:
+    df = pd.read_csv("../data/e-commerce_data.csv", encoding = "ISO-8859-1")
+    conn = sql.connect("../data/e-commerce.db")
+    df.to_sql("orders", conn, if_exists="replace", index=False)
+except Exception as e:
+    print(e)
+
+result_df = pd.read_sql("SELECT * FROM orders LIMIT 10", conn)
+
+
+
+"""Data cleaning"""
 # Basic data exploration for cleaning.
-print(df.head())
-print(df.info)
-print(df.isnull().sum())
-print(df.dtypes)
+# print(result_df)
+# print(df.head())
+# print(df.info())
+# print(df.isnull().sum()) # Description and CustomerID are missing data.
+# print(df.dtypes) # InvoiceDate change to date type.
+# print(df[df.duplicated()]) # 5268 rows of duplicates.
+
+
+# Cleaning missing data.
+df["Description"] = df["Description"].replace("", np.nan)
+df["CustomerID"] = df["CustomerID"].replace("", np.nan)
+
+
+# Converting date.
+df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"], format = "mixed", dayfirst = True)
+
+
+# Dropping duplicates.
+df = df.drop_duplicates()
+
+
+
+"""Exporting data"""
+# Saving results as a new table in SQL db and dropping connection.
+try:
+    df.to_sql("orders_cleaned", conn, if_exists="replace", index = False)
+    conn.close()
+except Exception as e:
+    print(e)
