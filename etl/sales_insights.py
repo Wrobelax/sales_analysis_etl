9171cde4cea60f6acdf97d27bd6f66aa94819287
Adrian <awrobel.wr@gmail.com>
2025-08-05 16:56:25 +0200

    Completed visualization.

--- etl/sales_insights.py
+++ etl/sales_insights.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.ticker as ticker
+import seaborn as sns
 
 
 
@@ -51,12 +52,12 @@ except Exception as e:
 
 """Data Visualization"""
 # ===Monthly orders===
-data = df_pivot.sum(axis=0)
+data = df_pivot.sum(axis = 0)
 normalize = cm.colors.Normalize(vmin = data.min(), vmax = data.max())
 colors = cm.plasma(normalize(data.values))
 
-plt.figure(figsize=(10,6))
-plt.bar(data.index, data.values, color=colors)
+plt.figure(figsize = (10, 6))
+plt.bar(data.index, data.values, color = colors)
 plt.title("Sum of monthly orders")
 plt.xlabel("Month")
 plt.ylabel("Orders")
@@ -134,4 +135,133 @@ plt.title("Top 10 sold products")
 plt.xlabel("Quantity")
 
 plt.tight_layout()
-plt.savefig("../outputs/top_products.png")
+# plt.savefig("../outputs/top_products.png")
+
+
+
+# ===Weekly sale===
+df_orders["InvoiceDate"] = pd.to_datetime(df_orders["InvoiceDate"])
+
+df_orders = df_orders.set_index("InvoiceDate")
+
+weekly_sales = df_orders["OrderValue"].resample("W").sum()
+weekly_sales = weekly_sales[weekly_sales > 0]
+
+plt.figure(figsize=(10,6))
+plt.plot(weekly_sales.index,
+         weekly_sales.values,
+         marker = "o",
+         linestyle = "-",
+         color = "purple",
+         label = "Sales over time"
+         )
+
+plt.title("Weekly sale per order value")
+plt.xlabel("Week")
+plt.ylabel("Total order value")
+plt.xticks(rotation = 45)
+
+plt.tight_layout()
+plt.grid(True)
+plt.savefig("../outputs/orders_daily.png")
+
+
+
+# ===Hourly Activity===
+df_orders = df_orders.reset_index()
+
+plt.figure(figsize=(10,6))
+plt.hist(df_orders["Hour"],
+         bins = 24,
+         edgecolor = "black",
+         color = "skyblue",
+         )
+
+plt.title("Customer activity by hour")
+plt.xlabel("Hour of day")
+plt.ylabel("Number of orders")
+plt.xticks(range(0, 24))
+plt.grid(axis = "y", linestyle = "--", alpha = 0.7)
+
+plt.tight_layout()
+# plt.savefig("../outputs/orders_by_hour.png")
+
+
+
+# ===Clients segments===
+segments = df_orders.groupby("ClientSegment")["CustomerID"].nunique()
+segments_drop = segments.drop("Other", errors = "ignore")
+
+fig, (ax1, ax2) = plt.subplots(1,2, figsize = (16,8))
+
+explode = [0, 0, 0, 0.2, 0, 0]
+
+
+wedges, texts, autotexts = ax1.pie(segments_drop.values,
+        labels = segments_drop.index,
+        autopct = "%1.1f%%",
+        colors = plt.cm.Pastel2.colors,
+        explode = explode,
+        startangle = 140,
+        wedgeprops={'edgecolor': 'black'}
+        )
+
+ax1.set_title("Client segment (excluding 'other')")
+
+bars = ax2.bar(segments.index,
+               segments.values,
+               color = "lightgreen",
+               edgecolor = "black")
+
+ax2.set_title("Number of clients in each segment")
+ax2.set_xlabel("Segment")
+ax2.set_ylabel("Number of clients")
+
+plt.tight_layout()
+plt.savefig("../outputs/segments.png")
+
+
+
+# ===Distribution of orders value===
+plt.figure(figsize = (10, 6))
+
+plt.hist(df_orders["OrderValue"],
+         bins = 50,
+         color = "red",
+         edgecolor = "black",
+         log = True)
+
+plt.title("Order value distribution")
+plt.xlabel("Order value")
+plt.ylabel("Number of orders")
+
+formatter = ticker.ScalarFormatter(useMathText = False)
+formatter.set_scientific(False)
+plt.gca().yaxis.set_major_formatter(formatter)
+
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("../outputs/order_value_distribution.png")
+
+
+
+# ===Top 10 clients per order value===
+plt.figure(figsize = (10, 6))
+df_orders = df_orders.reset_index()
+top_10_clients = df_orders.groupby("CustomerID")["OrderValue"].sum()
+top_10_clients = top_10_clients.sort_values(ascending = False).head(10)
+
+top_10_clients.index = top_10_clients.index.astype(str)
+
+plt.bar(top_10_clients.index,
+        top_10_clients.values,
+        color = "darkgreen",
+        edgecolor = "black")
+
+plt.title("Top 10 loyal customers")
+plt.xlabel("CustomerID")
+plt.ylabel("Value")
+
+plt.tight_layout()
+plt.savefig("../outputs/top_10_clients.png")
+plt.show()
